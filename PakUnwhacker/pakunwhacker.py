7e53d3803b2e5b67f0637b16fd92c3c293828b37
Jill <scrunguscrungus@gmail.com>
2018-01-26 20:20:08 +0000

    PAKUnwhacker: Updated extract_file, added info (See desc.)
    An info command has been added to use on PAK files. This command will print the number of files in the PAK and the initial file offset.
    
    The extract_file command has been updated and now accepts a file number as well as a filename.

--- PakUnwhacker/pakunwhacker.py
+++ PakUnwhacker/pakunwhacker.py
@@ -2,6 +2,14 @@ from sprint_pak import *
 import os
 import sys
 
+
+def IsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 try:
     inputFileName = sys.argv[1]
 except IndexError:
@@ -10,8 +18,9 @@ except IndexError:
     print("")
     print("Usage:")
     print("pakunwhacker.py <PAK File> list - Lists files inside a Sprint PAK")
+    print("pakunwhacker.py <PAK File> info - Display information about a Sprint PAK")
     print("pakunwhacker.py <PAK File> extract <folder> - Extracts all files from a Sprint PAK into a given folder")
-    print("pakunwhacker.py <PAK File> extract_file <file> - Extracts a specific file from a Sprint PAK")
+    print("pakunwhacker.py <PAK File> extract_file <file/file number> - Extracts a specific file from a Sprint PAK")
     exit()
 
 
@@ -25,6 +34,11 @@ if len(sys.argv) == 3:
             print( "File " + str(i) )
             print("File name: " + inputFile.file_list[i].filename)
             print("File size: " + str(inputFile.file_list[i].filesize) + " bytes")
+
+    if sys.argv[2] == "info":
+        print("Sprint PAK File")
+        print("Files in PAK: " + str(inputFile.header.file_count))
+        print("Initial file offset: " + str(inputFile.header.initial_file_offset))
 
 if len(sys.argv) == 4:
     if sys.argv[2] == "extract":
@@ -43,19 +57,37 @@ if len(sys.argv) == 4:
             f.close()
 
     if sys.argv[2] == "extract_file":
-        print("Extracting " + sys.argv[3] + " from " + inputFileName)
+        if IsInt(sys.argv[3]) == True:
+            print("Extracting File Number " + sys.argv[3] + " from " + inputFileName)
+        else:
+            print("Extracting " + sys.argv[3] + " from " + inputFileName)
         fileFound = False
         for i in range(inputFile.header.file_count):
-            if inputFile.file_list[i].filename.lower() == sys.argv[3].lower():
-                fileFound = True
-                print( "File " + str(i) )
-                print("File name: " + inputFile.file_list[i].filename)
-                print("File size: " + str(inputFile.file_list[i].filesize) + " bytes")
-                print("Writing to " + inputFile.file_list[i].filename)
-                f = open(inputFile.file_list[i].filename, 'wb')
-                f.write(inputFile.file_list[i].file_data)
-                f.close()
+            if IsInt(sys.argv[3]) == True:
+                if i == int(sys.argv[3]):
+                    fileFound = True
+                    print( "File " + str(i) )
+                    print("File name: " + inputFile.file_list[i].filename)
+                    print("File size: " + str(inputFile.file_list[i].filesize) + " bytes")
+                    print("Writing to " + inputFile.file_list[i].filename)
+                    f = open(inputFile.file_list[i].filename, 'wb')
+                    f.write(inputFile.file_list[i].file_data)
+                    f.close()
+            else:
+                if inputFile.file_list[i].filename.lower() == sys.argv[3].lower():
+                    fileFound = True
+                    print( "File " + str(i) )
+                    print("File name: " + inputFile.file_list[i].filename)
+                    print("File size: " + str(inputFile.file_list[i].filesize) + " bytes")
+                    print("Writing to " + inputFile.file_list[i].filename)
+                    f = open(inputFile.file_list[i].filename, 'wb')
+                    f.write(inputFile.file_list[i].file_data)
+                    f.close()
+
         if fileFound != True:
-            print("Error!! File " + sys.argv[3] + " not found in " + inputFileName)
+            if IsInt(sys.argv[3]) == True:
+                print("Error!! File number " + sys.argv[3] + " not found in " + inputFileName)
+            else:
+                print("Error!! File " + sys.argv[3] + " not found in " + inputFileName)
         else:
             print("Extraction successful!")
